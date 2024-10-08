f63a4e43ec050305c1ad769f6e21bd7004aee2dd
pranjal <pranjalkale677@gmail.com>
2024-10-08 14:01:20 +0530

    update

---
+++ client/src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+import {Link} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+function Navbar() {
+    return(
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark"></nav>
+        );
+
+    
+
+}
+export default Navbar;
